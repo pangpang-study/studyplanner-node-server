1786a6011bc1d9353fe2dddec0d6e702704d1dec
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-08-13 01:02:58 +0900

    server initial setup

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const mainRouter = require('./routes/main');
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.set('port', process.env.PORT || 5000);
+
+app.use('/', mainRouter);
+
+app.use((err, req, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
-app.listen(5000);
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.send('error!');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
